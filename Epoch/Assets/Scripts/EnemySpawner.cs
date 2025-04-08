69cf08a87cba852fdba017af15dcc6b4a9404d7d
anthongron <alovegren0727@gmail.com>
2025-04-08 17:51:27 -0400

    added randomized enemy spawn locations

---
+++ Epoch/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public int numberOfEnemies = 1;
+    public GameObject player;
+
+    public PolygonCollider2D spawnArea;
+
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    void SpawnEnemies()
+    {
+        Bounds bounds = spawnArea.bounds;
+
+        int spawned = 0;
+        int maxAttempts = numberOfEnemies * 100; // prevent infinite loop
+        int attempts = 0;
+
+        while (spawned < numberOfEnemies && attempts < maxAttempts)
+        {
+            attempts++;
+            Vector2 randomPoint = new Vector2(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y)
+            );
+
+            if (spawnArea.OverlapPoint(randomPoint))
+            {
+                GameObject enemyInstance = Instantiate(enemyPrefab, randomPoint, Quaternion.identity);
+                AIDestinationSetter destSetter = enemyInstance.GetComponent<AIDestinationSetter>();
+                if (destSetter != null)
+                {
+                    destSetter.target = player.transform;
+                }
+                spawned++;
+            }
+        }
+    }
+}
